424122ae5a68c3aa2668f56811e1a42232b7a442
Hugo Marinho <hugomsfh@hotmail.com>
2022-03-02 15:18:27 +0100

    CHANGELOG - Change the location of the footer - Added background colors to the operato buttons

--- main.js
+++ main.js
@@ -1,18 +1,47 @@
+//Listen to keys pressed on the keyboard
 window.addEventListener('keydown', writeScreen);
 
+window.addEventListener()
+//function to change colors of cells that are hovered
+function changeBackColor(){
+    $('.cell').hover( 
+        function() {
+            $(this)
+            .css('color','blue')
+            .animate({'color': 'red'}, 400);
+        },
+        function() {
+            $(this)
+            .animate({'color': 'blue'}, 400);
+        }
+    );
+}
+
+//Check if typed key is a number
+//if it is, then write it on the calculator
+//screen
 function writeScreen(e){
     let aux1 = parseFloat(e.key);
     let screenValue = document.querySelector('.calcScreen');
-
-    console.log(screenValue.textContent);
 
     if(aux1 >=0 && aux1 <=9){
         console.log("You clicked on a number.");
         screenValue.textContent.trim() === "0" ? 
             screenValue.textContent = aux1 : screenValue.textContent += aux1;
+            //Still need to change the size of the font dinnamically as string grows
     }
     
 }
+
+//does not work......
+// function resize_to_fit() {
+//     var fontsize = $('.hiddenCont .calcScreen').css('font-size');
+//     $('.hiddenCont .calcScreen').css('fontSize', parseFloat(fontsize) - 1);
+  
+//     if ($('.hiddenCont .calcScreen').width() >= $('.hiddenCont').width()) {
+//       resize_to_fit();
+//     }
+//   }
 
 //Add sumbers
 function add (var1, var2){
